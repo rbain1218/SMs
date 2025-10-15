9a5ec6ae7e81b3a2fd45e7690cde134d4e105824
Rahul Bain <rahul@Rahuls-MacBook-Air.local>
2025-10-15 22:51:52 +0530

    change the database sql3_lite to postgresql

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'school_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school_db',
+        'USER': 'rahul',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
